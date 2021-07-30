062741209010a8d90a99fdfe0d9b61fb718ee402
Ajaykmr1328 <69295977+Ajaykmr1328@users.noreply.github.com>
2021-07-30 20:11:31 +0530

    Create Day 1: Data Types.js

---
+++ 30 Days of Code/Day 1: Data Types.js
@@ -0,0 +1,11 @@
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    let integer=readLine();
+    let decimal=readLine();
+    let string=readLine();
+    console.log(i+Number(integer));
+    console.log((d+Number(decimal)).toFixed(1));
+    console.log(s+string);
+}
