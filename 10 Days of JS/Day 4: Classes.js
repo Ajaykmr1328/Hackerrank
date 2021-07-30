2be5e305ff039bb2d2fffcde0fef4a5042c5d60e
Ajaykmr1328 <69295977+Ajaykmr1328@users.noreply.github.com>
2021-07-30 20:09:19 +0530

    Create Day 4: Classes.js

---
+++ 10 Days of JS/Day 4: Classes.js
@@ -0,0 +1,12 @@
+class Polygon{
+    constructor(heights){
+        let polygon_perimeter=0;
+        for(var index in heights){
+            polygon_perimeter+=heights[index];
+        }
+        this.my_perimeter=polygon_perimeter;
+    }
+    perimeter(){
+        return this.my_perimeter;
+    }
+}
