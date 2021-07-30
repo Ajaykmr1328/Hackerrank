636e62180bdbd6970a11e2c89594129d664a8187
Ajaykmr1328 <69295977+Ajaykmr1328@users.noreply.github.com>
2021-07-30 19:58:40 +0530

    Create Day 0: Data Types.js

---
+++ 10 Days of JS/Day 0: Data Types.js
@@ -0,0 +1,8 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    let firstInteger = 4;
+    let firstDecimal = 4.0;
+    let firstString = 'HackerRank ';
+    console.log(firstInteger+Number(secondInteger));
+    console.log(firstDecimal+parseFloat(secondDecimal));
+    console.log(firstString+secondString);
+}
